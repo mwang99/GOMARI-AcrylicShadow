433f8e675cfcc49a4057a39254dc7e9065161061
maxion <vegito.wang@gmail.com>
2015-05-18 10:00:17 -0500

    Update RegisterButtons.java

--- GUI/RegisterButtons.java
+++ GUI/RegisterButtons.java
@@ -8,15 +8,24 @@ class RegisterButtons extends JPanel {
 		
 		welcomeLabel = new JLabel("Register Buttons");
 		
+		firstNameArea = new JTextArea("");
+		lastNameArea = new JTextArea("");
+		idArea = new JTextArea("");
+		
 		confirmButton = new JButton("Confirm Registration");	
 		
 		add(welcomeLabel);
+		add(firstNameArea);
+		add(lastNameArea);
+		add(idArea);
 		add(confirmButton);
 		
 	}
 
 private JLabel welcomeLabel;
 private JButton confirmButton;
-
+private JTextArea firstNameArea;
+private JTextArea lastNameArea;
+private JTextArea idArea;
 	
 }
