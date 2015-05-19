a6da756640b42b04678fce175641f9ce93f18a0c
maxion <vegito.wang@gmail.com>
2015-05-19 09:50:22 -0500

    Create ClockInOut.java

---
+++ GUI/ClockInOut.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+class ClockInOut extends JFrame {
+	
+	public ClockInOut(){
+	
+		JFrame frame = new JFrame("Clocking - In/Out");
+		frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		frame.getContentPane().add(new ClockButtons());
+		frame.setSize(300,100);	
+		frame.setVisible(true);	
+			
+	}
+}
