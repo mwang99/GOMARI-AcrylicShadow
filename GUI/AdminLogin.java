a823f1150adf328b927cbe64a578ff9070bfeb31
maxion <vegito.wang@gmail.com>
2015-05-20 09:46:35 -0500

    Update AdminLogin.java

--- GUI/AdminLogin.java
+++ GUI/AdminLogin.java
@@ -8,6 +8,7 @@ class AdminLogin extends JFrame {
 		frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		frame.getContentPane().add(new AdminLoginButtons());
 		frame.setSize(300,75);	
+		frame.pack();
 		frame.setVisible(true);	
 			
 	}
