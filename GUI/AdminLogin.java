8746b9ac31b1929a940a99069f1f6498bbb37824
maxion <vegito.wang@gmail.com>
2015-05-20 09:14:54 -0500

    Create AdminLogin.java

---
+++ GUI/AdminLogin.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+class AdminLogin extends JFrame {
+	
+	public AdminLogin(){
+	
+		JFrame frame = new JFrame("Login");
+		frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		frame.getContentPane().add(new AdminLoginButtons());
+		frame.setSize(300,75);	
+		frame.setVisible(true);	
+			
+	}
+}
