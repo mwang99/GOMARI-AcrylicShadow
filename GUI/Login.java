5a60d1bfe5f51bced44f860c3f8149a3c77ca54a
maxion <vegito.wang@gmail.com>
2015-05-20 09:47:00 -0500

    Update Login.java

--- GUI/Login.java
+++ GUI/Login.java
@@ -7,7 +7,8 @@ class Login extends JFrame {
 		JFrame frame = new JFrame("Login");
 		frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		frame.getContentPane().add(new LoginButtons());
-		frame.setSize(300,75);	
+		frame.setSize(300,75);
+		frame.pack();	
 		frame.setVisible(true);	
 			
 	}
