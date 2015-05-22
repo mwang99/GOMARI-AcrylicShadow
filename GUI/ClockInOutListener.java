32c62d2589415c5b60bfc44636ce71344302c552
maxion <vegito.wang@gmail.com>
2015-05-22 11:25:58 -0500

    Create ClockInOutListener.java

---
+++ GUI/ClockInOutListener.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class ClockInOutListener implements ActionListener {
+	
+	public void actionPerformed (ActionEvent event) {
+			new ClockInOut();
+	}	
+		
+}
+
