18108de3964fb21df6e3e8594e88a35725948ec0
maxion <vegito.wang@gmail.com>
2015-05-22 11:25:16 -0500

    Create LoginListener.java

---
+++ GUI/LoginListener.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class LoginListener implements ActionListener {
+	
+	public void actionPerformed (ActionEvent event) {
+			new Login();
+	}	
+		
+}
+
