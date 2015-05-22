d89df2ccc509a79c50ff4cf221ddf073057c5d74
maxion <vegito.wang@gmail.com>
2015-05-22 11:26:30 -0500

    Create AdminLoginListener.java

---
+++ GUI/AdminLoginListener.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class AdminLoginListener implements ActionListener {
+	
+	public void actionPerformed (ActionEvent event) {
+			new AdminLogin();
+	}	
+		
+}
+
