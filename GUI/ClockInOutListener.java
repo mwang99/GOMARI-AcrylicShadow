49b086949f24159c823bc519929610bb839b156c
maxion <vegito.wang@gmail.com>
2015-05-26 09:54:40 -0500

    Update ClockInOutListener.java

--- GUI/ClockInOutListener.java
+++ GUI/ClockInOutListener.java
@@ -2,10 +2,10 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-class ClockInOutListener implements ActionListener {
+class ClockInListener implements ActionListener {
 	
 	public void actionPerformed (ActionEvent event) {
-			new ClockInOut();
+
 	}	
 		
 }
