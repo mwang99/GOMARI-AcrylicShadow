02b2d99c3fe373731855efbbc94fc804769e5d7d
maxion <vegito.wang@gmail.com>
2015-05-26 09:55:33 -0500

    Update GUI.java

--- GUI/GUI.java
+++ GUI/GUI.java
@@ -5,7 +5,7 @@ class GUI extends JFrame {
 	public GUI(){
 		
 		JFrame frame = new JFrame("Acrylic Shadow // GOMARI");
-		frame.setDefaultCloseOperation (JFrame.HIDE_ON_CLOSE);
+		frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().add(new Buttons());
 		frame.setSize(600,400);	
 		frame.setVisible(true);	
