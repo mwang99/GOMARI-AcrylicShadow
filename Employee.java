94ec760471715b7946aec815eff8a9a39a4d8fdb
GowthamVenkat99 <gowthamvenkat99@gmail.com>
2015-05-26 09:55:17 -0500

    Update and rename Employee to Employee.java

---
+++ Employee.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class Employee here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Employee extends Person
+{
+    private ArrayList<Data> attendance;
+    private int idNum;
+    private int dataNum;
+    private static Final Date  ;
+    /**
+     * Constructor for objects of class Employee
+     */
+    public Employee(String firstName, String lastName, int tempIdNum)
+    {
+        super(firstName, lastName, tempIdNum);
+        dataNum = 0;
+    }
+
+    /**
+     */
+    public void clockIn()
+    {
+        Data temp = new Data() ;
+        temp.checkInTime();
+        attendance.add(temp);
+    }
+    
+     /**
+     */
+    public void clockOut()
+    {
+        attendance.get(dataNum).checkOutTime();
+    }
+}
+
