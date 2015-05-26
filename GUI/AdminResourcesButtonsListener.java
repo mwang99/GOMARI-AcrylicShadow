3e7e7a641c3851840edc5d1452ac158739db6524
ridwanc12 <rchowdhury1212@gmail.com>
2015-05-26 09:56:07 -0500

    Create AdminResourcesButtonsListener.java

---
+++ GUI/AdminResourcesButtonsListener.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class AdminResourcesButtonsListener implements ActionListener {
+	
+	public void actionPerformed (ActionEvent event) {
+	}	
+		
+}
+
+
