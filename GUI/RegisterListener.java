458a654b606b63250939543b82c861cb2613c8d5
ridwanc12 <rchowdhury1212@gmail.com>
2015-05-27 09:22:03 -0500

    Update RegisterListener.java

--- GUI/RegisterListener.java
+++ GUI/RegisterListener.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-class ButtonsListener implements ActionListener {
+class RegisterListener implements ActionListener {
 	
 	public void actionPerformed (ActionEvent event) {
 			new Register();
