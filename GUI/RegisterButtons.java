a1655e6eadb833afad83f987c5679c540a318719
GowthamVenkat99 <gowthamvenkat99@gmail.com>
2015-05-27 09:46:42 -0500

    Update RegisterButtons.java

--- GUI/RegisterButtons.java
+++ GUI/RegisterButtons.java
@@ -2,42 +2,39 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-class RegisterButtons extends JPanel implements ActionListener{
-	
-	public RegisterButtons(){
-		
-		welcomeLabel = new JLabel("Register");
-		
-		LoginListener l = new LoginListener();
-		
-		firstNameArea = new JTextField(12);
-		lastNameArea = new JTextField(12);
-		idArea = new JTextField(12);
-		
-		firstNameLabel = new JLabel("First Name:");
-		lastNameLabel = new JLabel("Last Name: ");
-		idLabel = new JLabel("ID: ");
-		
-		confirmButton = new JButton("Confirm Registration");	
-		String firstName = firstNameLabel.getText().toString();
-		String lastName = lastNameLabel.getText().toString();
-		int idNum = Integer.parseInt(firstNameLabel.getText().toString());
-		//confirmButton.addActionListener();
-		confirmButton.addActionListener(l);
-		
-		add(welcomeLabel);
-		add(firstNameLabel);
-		add(firstNameArea);
-		add(lastNameLabel);
-		add(lastNameArea);
-		add(idLabel);
-		add(idArea);
-		add(confirmButton);
-		
-	}
-
-	public void actionPerformed(ActionEvent e) { 
-       System.out.println("Cat");
+class RegisterButtons extends JPanel {
+    
+    public RegisterButtons(){
+        
+        welcomeLabel = new JLabel("Register");
+        
+        LoginListener l = new LoginListener();
+        //RegisterPersonListener rp = new RegisterPersonListener();
+        
+        firstNameArea = new JTextField(12);
+        lastNameArea = new JTextField(12);
+        idArea = new JTextField(12);
+        
+        firstNameLabel = new JLabel("First Name:");
+        lastNameLabel = new JLabel("Last Name: ");
+        idLabel = new JLabel("ID: ");
+        
+        confirmButton = new JButton("Confirm Registration");    
+        firstName = firstNameLabel.getText().toString();
+        lastName = lastNameLabel.getText().toString();
+        idNum = Integer.parseInt(firstNameLabel.getText().toString());  
+        //confirmButton.addActionListener(rp);
+        confirmButton.addActionListener(l);
+        
+        add(welcomeLabel);
+        add(firstNameLabel);
+        add(firstNameArea);
+        add(lastNameLabel);
+        add(lastNameArea);
+        add(idLabel);
+        add(idArea);
+        add(confirmButton);
+        
     }
 private JLabel welcomeLabel;
 private JLabel firstNameLabel;
@@ -47,5 +44,7 @@ private JButton confirmButton;
 private JTextField firstNameArea;
 private JTextField lastNameArea;
 private JTextField idArea;
-	
+protected String firstName;
+protected String lastName;
+protected int idNum;
 }
